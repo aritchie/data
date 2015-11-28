62888423b59e2e8afa0b5a4c65caac925fc48bb5
Allan Ritchie <allan.ritchie@gmail.com>
2015-11-28 14:53:27 -0500

    Add remove and a couple of new potential plugin points

--- Acr.Data.Ef7/Properties/AssemblyInfo.cs
+++ Acr.Data.Ef7/Properties/AssemblyInfo.cs
@@ -1,6 +1,10 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Acr.Data.Ef7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -10,7 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: Guid("063fa464-f7a1-4763-92a7-462436e3795e")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b544a48a-8c07-4a9d-9577-e885ea995931")]
--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -1,5 +1,5 @@
 @echo off
 del *.nupkg
-nuget pack Acr.Data.nuspec
+rem nuget pack Acr.Data.nuspec
 nuget pack Acr.Data.Ef.nuspec
 pause
