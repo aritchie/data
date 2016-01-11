a60e88c2ae9e1c7a1738fbfed0587cb189038a6c
Allan Ritchie <allan.ritchie@gmail.com>
2016-01-11 08:51:06 -0500

    MongoDB implementation

---
+++ Acr.Data.Mongo/MongoDataContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+
+namespace Acr.Data.Mongo {
+
+    public class MongoDataContext : IDataContext {
+        readonly IMongoDatabase data; // should be static
+
+
+        public MongoDataContext(IMongoDatabase data) {
+            this.data = data;
+        }
+
+
+        public T Add<T>(T obj) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync<T>(IQueryable<T> query) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+            return this.data
+                .GetCollection<T>(typeof(T).Name)
+                .AsQueryable()
+                .Where(expression)
+                .AnyAsync();
+        }
+
+        public Task<int> CountAsync<T>(IQueryable<T> query) {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> query) {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(object id) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync<T>(object id) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Query<T>() where T : class {
+            throw new NotImplementedException();
+        }
+
+        public void Remove<T>(T entity) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges() {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync() {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SingleOrDefaultAsync<T>(IQueryable<T> query) {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ToListAsync<T>(IQueryable<T> query) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ToListAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Acr.Data.Mongo/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Acr.Data.Mongo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acr.Data.Mongo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c85882ad-036d-44ca-92bc-5e18a53ec7b4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
