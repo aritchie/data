8db9c7ed2cd5222463ee71e471efa8c5f1a5d96d
Allan Ritchie <allan.ritchie@gmail.com>
2016-02-04 07:51:23 -0500

    Minor updates to ef7 context

--- Acr.Data.Ef7/EfDataContext.cs
+++ Acr.Data.Ef7/EfDataContext.cs
@@ -6,57 +6,72 @@ using System.Threading.Tasks;
 using Microsoft.Data.Entity;
 
 
-namespace Acr.Data.Ef {
+namespace Acr.Data.Ef
+{
 
-    public class EfDataContext : IDataContext {
+    public class EfDataContext : IDataContext
+    {
         readonly DbContext data;
 
 
-        public EfDataContext(DbContext data) {
+        public EfDataContext(DbContext data)
+        {
             this.data = data;
         }
 
 
-        public void SaveChanges() {
+        public void SaveChanges()
+        {
             this.data.SaveChanges();
         }
 
 
-        public Task SaveChangesAsync() {
+        public Task SaveChangesAsync()
+        {
             return this.data.SaveChangesAsync();
         }
 
 
-        public T Add<T>(T obj) where T : class {
+        public T Add<T>(T obj) where T : class
+        {
             return this.data.Set<T>().Add(obj).Entity;
         }
 
 
-        public IQueryable<T> Query<T>() where T : class {
+        public IQueryable<T> Query<T>() where T : class
+        {
             return this.data.Set<T>();
         }
 
 
-        public T Get<T>(object id) where T : class {
-            return this.data.Set<T>().;
+        public T Get<T>(object id) where T : class
+        {
+            var model = this.data.Model
+                .GetEntityTypes()
+                .First(x => x.ClrType == typeof(T));
+            return this.data.Set<T>().FirstOrDefault();
         }
 
-        public void Remove<T>(T entity) where T : class {
+        public void Remove<T>(T entity) where T : class
+        {
             this.data.Set<T>().Remove(entity);
         }
 
 
-        public Task<T> GetAsync<T>(object id) where T : class {
+        public Task<T> GetAsync<T>(object id) where T : class
+        {
             return this.data.Set<T>().FindAsync(id);
         }
 
 
-        public async Task<IEnumerable<T>> ToListAsync<T>(IQueryable<T> query) {
+        public async Task<IEnumerable<T>> ToListAsync<T>(IQueryable<T> query)
+        {
             return await query.ToListAsync();
         }
 
 
-        public async Task<IEnumerable<T>> ToListAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+        public async Task<IEnumerable<T>> ToListAsync<T>(Expression<Func<T, bool>> expression) where T : class
+        {
             return await this.data
                 .Set<T>()
                 .Where(expression)
@@ -64,48 +79,56 @@ namespace Acr.Data.Ef {
         }
 
 
-       public Task<T> SingleOrDefaultAsync<T>(IQueryable<T> query) {
+        public Task<T> SingleOrDefaultAsync<T>(IQueryable<T> query)
+        {
             return query.SingleOrDefaultAsync();
         }
 
 
-        public Task<T> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+        public Task<T> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class
+        {
             return this.data
                 .Set<T>()
                 .SingleOrDefaultAsync(expression);
         }
 
 
-        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> query) {
+        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> query)
+        {
             return query.FirstOrDefaultAsync();
         }
 
 
-        public Task<T> FirstOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+        public Task<T> FirstOrDefaultAsync<T>(Expression<Func<T, bool>> expression) where T : class
+        {
             return this.data
                 .Set<T>()
                 .FirstOrDefaultAsync(expression);
         }
 
 
-        public Task<int> CountAsync<T>(IQueryable<T> query) {
+        public Task<int> CountAsync<T>(IQueryable<T> query)
+        {
             return query.CountAsync();
         }
 
 
-        public Task<int> CountAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+        public Task<int> CountAsync<T>(Expression<Func<T, bool>> expression) where T : class
+        {
             return this.data
                 .Set<T>()
                 .CountAsync(expression);
         }
 
 
-        public Task<bool> AnyAsync<T>(IQueryable<T> query) {
+        public Task<bool> AnyAsync<T>(IQueryable<T> query)
+        {
             return query.AnyAsync();
         }
 
 
-        public Task<bool> AnyAsync<T>(Expression<Func<T, bool>> expression) where T : class {
+        public Task<bool> AnyAsync<T>(Expression<Func<T, bool>> expression) where T : class
+        {
             return this.data
                 .Set<T>()
                 .AnyAsync(expression);
